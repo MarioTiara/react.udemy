57638ee7c1370d4930c0ea66d143079705d50ea1
MarioTiara <mario.tiarapratama@gmail.com>
2023-09-15 01:26:44 +0700

    dynamic component data

--- React-Components/src/components/ExpenseItem.js
+++ React-Components/src/components/ExpenseItem.js
@@ -1,10 +1,15 @@
+import './ExpenseItem.css'
 function ExpenseItem(){
+    const expenseDate = new Date(2021,2,28)
+    const expensePrice ='$294.64'
+
+    const expenseTtitle ="Car Insurance"
     return (
-        <div>
-            <div>March 28th 2021</div>
-            <div>
-                <h2>Car Insurance</h2>
-                <div>$294.64</div>
+        <div className="expense-item">
+            <div>{expenseDate.toISOString()}</div>
+            <div className="expense-item__description ">
+                <h2 className='.expense-item h2'>{expenseTtitle}</h2>
+                <div className="expense-item__price ">{expensePrice}</div>
             </div>
         </div>
     )
