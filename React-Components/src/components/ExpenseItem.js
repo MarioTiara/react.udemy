e4b30384d8e984786b370838c035de19921ca13a
MarioTiara <mario.tiarapratama@gmail.com>
2023-09-15 02:02:40 +0700

    use date local format function

--- React-Components/src/components/ExpenseItem.js
+++ React-Components/src/components/ExpenseItem.js
@@ -1,8 +1,13 @@
 import './ExpenseItem.css'
 function ExpenseItem({date,title, price}){
+    const {month, day, year}=dateHelper(date)
     return (
         <div className="expense-item">
-            <div>{date.toISOString()}</div>
+            <div>
+                <div>{month}</div>
+                <div>{day}</div>
+                <div>{year}</div>
+            </div>
             <div className="expense-item__description ">
                 <h2 className='.expense-item h2'>{title}</h2>
                 <div className="expense-item__price ">{price}</div>
@@ -11,4 +16,12 @@ function ExpenseItem({date,title, price}){
     )
 }
 
+function dateHelper(date)
+{
+    const month = date.toLocaleString('en-US', {month:'long'})
+    const day= date.toLocaleString('en-US', {day:'2-digit'})
+    const year= date.getFullYear()
+
+    return {month:month, day:day, year:year}
+}
 export default ExpenseItem
