722511750732f8b42f57d3ac8a8cf4049f49961c
MarioTiara <mario.tiarapratama@gmail.com>
2023-09-15 01:37:32 +0700

    use destructing props

--- React-Components/src/App.js
+++ React-Components/src/App.js
@@ -4,7 +4,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   const date=new Date(2021,2,28)
   return (
-    <ExpenseItem title='Car Insurance' price='$294.64' date={date}/>
+    <ExpenseItem  date={date} title='Car Insurance' price='$294.64'/>
   );
 }
 
--- React-Components/src/components/ExpenseItem.js
+++ React-Components/src/components/ExpenseItem.js
@@ -1,11 +1,11 @@
 import './ExpenseItem.css'
-function ExpenseItem(props){
+function ExpenseItem({date,title, price}){
     return (
         <div className="expense-item">
-            <div>{props.date.toISOString()}</div>
+            <div>{date.toISOString()}</div>
             <div className="expense-item__description ">
-                <h2 className='.expense-item h2'>{props.title}</h2>
-                <div className="expense-item__price ">{props.price}</div>
+                <h2 className='.expense-item h2'>{title}</h2>
+                <div className="expense-item__price ">{price}</div>
             </div>
         </div>
     )
