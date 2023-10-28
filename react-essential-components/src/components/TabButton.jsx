99ed4643bdb2280901e7d3dda7bee773d25f74d2
MarioTiara <mario.tiarapratama@gmail.com>
2023-10-28 10:18:54 +0700

    [55] css Styling & Dynamic styling

--- react-essential-components/src/components/TabButton.jsx
+++ react-essential-components/src/components/TabButton.jsx
@@ -1,11 +1,13 @@
-const TabButton =({children, onSelect})=> {
+const TabButton =({children, onSelect, isSelected})=> {
     function handleClick(){
         console.log("hello mario")
     }
     
     
     return <li>
-        <button onClick={onSelect}>{children}</button>
+        <button className={isSelected?'active':undefined} onClick={onSelect}>
+            {children}
+        </button>
     </li>
 }
 
