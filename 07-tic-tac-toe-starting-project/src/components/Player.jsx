eff07348cc4fdf71b1499a8821c280d2e281dbbc
MarioTiara <mario.tiarapratama@gmail.com>
2023-10-30 21:01:03 +0700

    [81] Lifting State Up [Core Concept

--- 07-tic-tac-toe-starting-project/src/components/Player.jsx
+++ 07-tic-tac-toe-starting-project/src/components/Player.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-const Player = ({ initialName, symbol }) => {
+const Player = ({ initialName, symbol, isActive }) => {
   const [isEditing, setIsEditing] = useState(false);
   const [playername, setPlayerName] = useState(initialName);
   const handleEditClick = () => {
@@ -20,7 +20,7 @@ const Player = ({ initialName, symbol }) => {
   }
 
   return (
-    <li>
+    <li className={isActive? 'active' :undefined}>
       <span className="player">
         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
