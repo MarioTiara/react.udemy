d6412e86c55212807babe6f5745351739d175c37
MarioTiara <mario.tiarapratama@gmail.com>
2023-10-30 16:22:11 +0700

    [79] rendering multidemensional list

--- 07-tic-tac-toe-starting-project/src/App.jsx
+++ 07-tic-tac-toe-starting-project/src/App.jsx
@@ -1,6 +1,6 @@
 
 import Player from "./components/Player"
-
+import GameBoard from "./components/GameBoard"
 function App() {
   
 
@@ -11,7 +11,7 @@ function App() {
                   <Player initialName="Player 1"  symbol="X"></Player>
                   <Player initialName="Player 2"  symbol="O"></Player>
             </ol>
-            GAME BORD
+           <GameBoard/>
         </div>
     </main>
   )
